7d3cfcf4f4cb1fce84899323d3bc94ddf9e45a19
user <aga@example.com>
2018-06-13 19:53:21 +0200

    TaskController update

--- ToDo.App/Controllers/TaskController.cs
+++ ToDo.App/Controllers/TaskController.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ToDo.App.Controllers
 {
-    public class TaskController
+    [Route("api/[controller]")]
+    public class TaskController : Controller
     {
+        [HttpPost]
+        public void AddTask([FromBody])
+        {
+
+        }
+
+        public void DeleteTask()
+        {
+
+        }
+
+        public void GetTask()
+        {
+
+        }
+
+        public void PatchTask()
+        {
+
+        }
     }
 }
