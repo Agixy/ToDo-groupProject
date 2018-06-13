bda6d414a08c8bc35823844baf876cda3e4791a0
user <aga@example.com>
2018-06-13 20:38:49 +0200

    Controllers update

--- ToDo.App/Controllers/TaskController.cs
+++ ToDo.App/Controllers/TaskController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Rest.TransientFaultHandling;
+using Newtonsoft.Json;
+using ToDo.App.ViewModels;
 
 namespace ToDo.App.Controllers
 {
@@ -11,8 +13,16 @@ namespace ToDo.App.Controllers
     public class TaskController : Controller
     {
         [HttpPost]
-        public void AddTask([FromBody])
+        public void AddTask([FromBody] TaskDto task)
         {
+            if (task != null)
+            {
+                // add to base
+            }
+            else
+            {           
+                throw new HttpRequestWithStatusException("Pusty obiekt"){StatusCode = HttpStatusCode.BadRequest};
+            }
 
         }
 
@@ -21,9 +31,15 @@ namespace ToDo.App.Controllers
 
         }
 
-        public void GetTask()
+        [HttpGet]
+        public TaskDto GetTask()
         {
+            return new TaskDto();
+        }
 
+        public IEnumerable<TaskDto> GetTasks()
+        {
+            return new List<TaskDto>{new TaskDto(), new TaskDto()};
         }
 
         public void PatchTask()
