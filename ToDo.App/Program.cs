a1d36a1048e1b2f7468342fc2c4345675eab90c2
Konrad <konradgadecki@gmail.com>
2018-06-15 18:41:21 +0200

    Set up DB

--- ToDo.App/Program.cs
+++ ToDo.App/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using ToDo.DBConnection.DatabaseAccess;
+using ToDo.Model.Model;
 
 namespace ToDo.App
 {
@@ -13,16 +14,14 @@ namespace ToDo.App
         {
             //BuildWebHost(args).Run();
             var optionsBuilder = new DbContextOptionsBuilder<ServerContext>();
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ToDoDB;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer("Server=(local);Database=ToDoDB;Integrated Security=true;");
 
             using (var context = new ServerContext(optionsBuilder.Options))
-            { 
-                //var users = context.Users.ToList();
-                //foreach (var user in users)
-                //{
-                //    Console.WriteLine(user.Name);
-                //}
+            {
+
             }
+
+            Console.ReadKey();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
