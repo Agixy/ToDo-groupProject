d1bfca127a93c0ba5208c2a4c65ccfefc1a55cbf
Konrad <konradgadecki@gmail.com>
2018-06-17 00:58:40 +0200

    Deleted css reference

--- ToDo.App/ClientApp/app/components/fetchdata/fetchdata.component.ts
+++ ToDo.App/ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -5,8 +5,7 @@ import { Http } from '@angular/http';
 
 @Component({
     selector: 'fetchdata',
-    templateUrl: './fetchdata.component.html',
-    styleUrls: ['./fetchdata.component.css']
+    templateUrl: './fetchdata.component.html'
 })
 export class FetchDataComponent {
     public tasks: TaskDto[];
